b9be66b0e3435f34128e06ce32b6615d9d7ec111
Aaron Huggins <huggins.aaron@outlook.com>
2022-01-13 15:50:46 -0800

    adjusted routing

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/App.css';
 import LibraryContainer from './components/LibraryContainer';
-import {BrowserRouter} from "react-router-dom"
+//router
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
     <div className='outer-container'>
       <div className='inner-container'>
-        <BrowserRouter basename='/library'>
-          <LibraryContainer />
-        </BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <Routes>
+                <Route path='/' element={<LibraryContainer />}></Route>
+            </Routes>
+        </BrowserRouter> 
       </div>
     </div>
   </React.StrictMode>,
