1b4116340bb18faee025d10f4416a9eb6e165bc3
Aaron Huggins <huggins.aaron@outlook.com>
2022-01-13 15:06:43 -0800

    added redirect

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/App.css';
 import LibraryContainer from './components/LibraryContainer';
+import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
     <div className='outer-container'>
       <div className='inner-container'>
-        <LibraryContainer />
+        <Router basename={process.env.PUBLIC_URL}>
+          <LibraryContainer />
+        </Router>
       </div>
     </div>
   </React.StrictMode>,
